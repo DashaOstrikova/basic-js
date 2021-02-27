2cde9871d58d79590b34644692e7f35028a616f6
Dasha Ostrikova <dashka.ostrikova@gmail.com>
2021-02-28 02:13:19 +0300

    feat: implement transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  let newArr = [];
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] === '--discard-next'){
+      i++;
+    }
+    else if(arr[i] === '--discard-prev'){
+      if(i != 0){
+        newArr.splice(i-1,1);
+      }
+    }
+    else if(arr[i] === '--double-next'){
+      if(i != (arr.length -1)){
+        newArr = [...newArr, arr[i+1]];
+      }
+    }
+    else if(arr[i] === '--double-prev'){
+      if(i != 0){
+        if(arr[i-2] ==='--discard-next'){}
+        else{
+          newArr = [...newArr, arr[i-1]];
+        }
+      }
+    }
+    else{
+      newArr = [...newArr, arr[i]];
+    }
+  }
+  return newArr;
+}
