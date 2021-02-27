273cf41719c9f27e3187857763e9e0ed27c1b87a
Dasha Ostrikova <dashka.ostrikova@gmail.com>
2021-02-27 23:32:53 +0300

    feat: implement DreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,14 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  throw new CustomError('Not implemented');
   let dreamTeam = '';
-  let sortMembers = members.sort();
-  for(let i=0;i<members.length;i++){
-    if(typeof sortMembers[i] == "string"){
-      sortMembers[i].toUpperCase();
-      let name = sortMembers[i].split(); 
-      dreamTeam += name[0];
-    }
+  if(!members || !members.length) {
+    return false;
   }
-};
+  let strMembers = members.filter(str => typeof str === "string").map(name => name.toUpperCase().trim()[0]);
+  let sortMembers = strMembers.sort();
+  dreamTeam = sortMembers.join('');
+  return dreamTeam;
+}
