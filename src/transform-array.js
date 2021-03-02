2a1fe022e30fa8470b55155eb767189f481b4c10
Dasha Ostrikova <dashka.ostrikova@gmail.com>
2021-03-02 15:19:56 +0300

    fix: change for

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,30 +2,24 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
   let newArr = [];
+  if (!arr || !Array.isArray(arr)) {
+    throw new TypeError("Is not a array");
+  }
   for(let i=0; i<arr.length; i++){
-    if(arr[i] === '--discard-next'){
-      i++;
-    }
-    else if(arr[i] === '--discard-prev'){
-      if(i != 0){
-        newArr.splice(i-1,1);
-      }
+    if(arr[i-1] === '--discard-next' || arr[i+1] === '--discard-prev' || arr[i] === '--double-prev' && arr[i-2] === '--discard-next'){
     }
     else if(arr[i] === '--double-next'){
-      if(i != (arr.length -1)){
-        newArr = [...newArr, arr[i+1]];
+      if(i !== (arr.length -1)){
+        newArr.push(arr[i+1]);
       }
     }
     else if(arr[i] === '--double-prev'){
-      if(i != 0){
-        if(arr[i-2] ==='--discard-next'){}
-        else{
-          newArr = [...newArr, arr[i-1]];
-        }
+      if(i !== 0){
+        newArr.push(arr[i-1]);
       }
     }
-    else{
-      newArr = [...newArr, arr[i]];
+    else if(arr[i] !== '--discard-next' && arr[i] !== '--discard-prev'){
+      newArr.push(arr[i]);
     }
   }
   return newArr;
