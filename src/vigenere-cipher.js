4d932fc174f8b1c9ce882d699a5dcacc9fdb3d18
Dasha Ostrikova <dashka.ostrikova@gmail.com>
2021-03-05 01:27:32 +0300

    feat: implement VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,112 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modification = true) {
+    this.modification = modification;
+  }
+
+  encrypt(message, key) {
+    if(message === undefined || key === undefined){
+      throw new TypeError('message or key not set');
+    }
+    let cipher = [];
+    let j = 0;
+    let charCodeCipher = 0;
+    let keyArr = key.split('');
+    for(let i=0; i<message.length; i++){
+      let charCodeMessage = message.charCodeAt(i);
+      if(message[i] === ' ' || charCodeMessage < 65 || charCodeMessage > 90 && charCodeMessage < 97 || charCodeMessage > 122){
+        if(i <= keyArr.length){
+          keyArr.splice(i, 0, ' ');
+        }
+      }
+      else if(i === keyArr.length){
+        keyArr[keyArr.length] = key[j];
+        j++;
+        if(j > key.length - 1){
+          j = 0;
+        }
+      }
+    }
+    key = keyArr.join('');
+    for(let i=0; i<message.length; i++){
+      let charCodeMessage = message.charCodeAt(i);
+      let charCodeKey = key.charCodeAt(i);
+      if(message[i] === ' ' || charCodeMessage < 65 || charCodeMessage > 90 && charCodeMessage < 97 || charCodeMessage > 122){
+        cipher[i] = message[i];
+      }
+      else{
+        if(charCodeMessage >= 65 && charCodeMessage <= 90){
+          if(charCodeKey >= 65 && charCodeKey <= 90){
+            charCodeCipher = ((charCodeMessage + charCodeKey) % 26) + 65;
+          }
+          else{
+            charCodeCipher = ((charCodeMessage + (charCodeKey - 32)) % 26) + 65;
+          }
+        }
+        if(charCodeMessage >= 97 && charCodeMessage <= 122){
+          if(charCodeKey >= 65 && charCodeKey <= 90){
+            charCodeCipher = (((charCodeMessage - 32) + charCodeKey) % 26) + 65;
+          }
+          else{
+            charCodeCipher = (((charCodeMessage - 32) + (charCodeKey - 32)) % 26) + 65;
+          }
+        }
+        let charCipher = String.fromCharCode(charCodeCipher);
+        cipher[i] = charCipher;
+      }
+    }
+    if(this.modification === false){
+      cipher.reverse();
+    }
+    return cipher.join('');
+  }   
+
+  decrypt(message, key) {
+    if(message === undefined || key === undefined){
+      throw new TypeError('message or key not set');
+    }
+    let decipher = [];
+    let j = 0;
+    let charCodeDecipher = 0;
+    let keyArr = key.split('');
+    for(let i=0; i<message.length; i++){
+      let charCodeMessage = message.charCodeAt(i);
+      if(message[i] === ' ' || charCodeMessage < 65 || charCodeMessage > 90){
+        if(i <= keyArr.length){
+          keyArr.splice(i, 0, ' ');
+        }
+      }
+      else if(i === keyArr.length){
+        keyArr[keyArr.length] = key[j];
+        j++;
+        if(j > key.length - 1){
+          j = 0;
+        }
+      }
+    }
+    key = keyArr.join('');
+    for(let i=0; i<message.length; i++){
+      let charCodeMessage = message.charCodeAt(i);
+      let charCodeKey = key.charCodeAt(i);
+      if(message[i] === ' ' || charCodeMessage < 65 || charCodeMessage > 90){
+        decipher[i] = message[i];
+      }
+      else{
+        if(charCodeKey >= 65 && charCodeKey <= 90){
+          charCodeDecipher = ((charCodeMessage + 26 - charCodeKey) % 26) + 65;
+        }
+        else{
+          charCodeDecipher = ((charCodeMessage + 26 - (charCodeKey - 32)) % 26) + 65;
+        }
+        let charDecipher = String.fromCharCode(charCodeDecipher);
+        decipher[i] = charDecipher;
+      }
+    }
+    if(this.modification === false){
+      decipher.reverse();
+    }
+    return decipher.join('');
   }
 }
-
 module.exports = VigenereCipheringMachine;
